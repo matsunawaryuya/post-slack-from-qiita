e50330ed2c197d456a396b79c2b0a76a02c2ea96
matsunawaryuya <ruymtnw@gmail.com>
2021-03-07 23:15:47 +0900

    add: コメント

--- postSlack.py
+++ postSlack.py
@@ -2,7 +2,11 @@
 import slackweb
 import qiita
 
+# Slack送信準備
 slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
 
+# Slack送信用のテキスト作成
 text = qiita.createText()
+
+# Slackにtextを送信 
 slack.notify(text=text)
